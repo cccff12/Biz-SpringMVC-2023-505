be306fa0fe01d6b0c99072b87ecb9cfeae1a754e
cccff12 <ksun0430@naver.com>
2023-07-13 12:56:12 +0900

    db

---
+++ SpMVC_003_BBsV2/FileUpload.md
@@ -0,0 +1,39 @@
+# Spring Server Application File Upload
+
+- HTML 에서 파일을 선택하여 Server 로 업로드를 할 때, form의 POST 방식으로 업로드를 한다.
+- 원래 form에서 Server 로 보내는 데이터는 Text데이터이다. 이 Text 데이터는 http 프로토콜의 body(payload)에 담겨서 encoding되어 전송된다.
+- form 에서 `type = "file"` 형식의 input tag를 이용하여 BIN 파일을 업로드 하면 데이터가 body에 담기고 encoding이 되는데 이 과정에서 BIN파일이 깨져버린다.
+- 그래서 HTML 에서는 form 의 전송되는 데이터에 새로운 규격을 추가했다. Text type의 데이터와 None Text type데이터를 같이 묶음으로 하여 Server 로 전송할 수 있도록 form에 속성을 추가하였다.
+- form에`type=file` 속성이 있는 input tag가 있으면 form tag에 `enctype="multipart/form-data"`라는 속성을 첨가해야 한다. 
+- form `enctype="multipart/form-data"` 라는 속성이 첨가되어 있으면 HTML 데이터를 전송하는 http 프로토콜이 종전의 form 데이터를 전송하는 방법과 달리 작동한다.
+- form의 `enctype="multipart/form-data"`는 Text 데이터와 none Text 데이터를 하나의 프로토콜 payload에 묶어서 보내는 Html 규격이다.
+
+## Spring Controller 에서 multipart/form-data 받기
+- Spring Controller(Dispatcher Servlet) 는 form 에서 전달되는 모든 데이터를 Text 로만 인식한다.
+- form 에서 아무리 `multipart/form-data` 형식으로 데이터를 보내도 Spring Controller 에서는 그러한 데이터를 수신하지 못한다 
+- Spring Project 에서 파일 업로드 기능을 구현하기 위해서는 다음의 2가지 Dependecies를 설치해야 한다.
+- 현재 `commons-fileupload`최신버전인 1.5 버전을 사용할 경우는 `commons-io` 를 같이 설치하지 않아도 된다.
+- `commons-fileupload` `1.5` 버전을 설치하면 `commons-io` 2.11.0 버전이 의존성 관계에 적합한 도구로 자동 설치된다.
+
+```xml
+<!--2023.07.12 현재-->
+		<!-- https://mvnrepository.com/artifact/commons-fileupload/commons-fileupload -->
+		<dependency>
+			<groupId>commons-fileupload</groupId>
+			<artifactId>commons-fileupload</artifactId>
+			<version>1.5</version>
+		</dependency>
+
+
+<!-- https://mvnrepository.com/artifact/commons-io/commons-io -->
+<dependency>
+    <groupId>commons-io</groupId>
+    <artifactId>commons-io</artifactId>
+    <version>2.13.0</version>
+</dependency>
+
+```
+## Spring Project 에서 파일 업로드를 하기 위한 `context`(bean) 설정
+- 파일의 업로드 허용 크기를 지정하여 bean 을 하나 생성해 두면 된다.
+- 생성한 bean을 직접 핸들링하지 않고, 이 bean은 Dispatcher Servlet 이 알아서 사용하는 bean 이다.
+- `root-context.xml` 을 사용해 전역적(global public ) 세팅을 수행한다.
---
+++ SpMVC_003_BBsV2/src/main/java/com/callor/bbs/controller/HomeController.java
@@ -0,0 +1,92 @@
+package com.callor.bbs.controller;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.callor.bbs.dao.BBsDao;
+import com.callor.bbs.models.BBsDto;
+import com.callor.bbs.service.FileService;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+@Controller
+public class HomeController {
+
+	
+	private final FileService fileService;
+	private final BBsDao bbsDao;
+
+	public HomeController(FileService fileService, BBsDao bbsDao) {
+		super();
+		this.fileService = fileService;
+		this.bbsDao = bbsDao;
+	}
+
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+//		List<BBsDto> bbsList = bbsDao.selectAll();
+		List<BBsDto> bbsList = bbsDao.findUserSelectLimit();
+		model.addAttribute("BBS_LIST", bbsList);
+		return "home";
+	}
+	
+	
+	@RequestMapping(value = "/insert",method = RequestMethod.GET)
+	public String insert() {
+		return "input";
+	}
+	
+	
+	/*
+	 * @RequestParam(value ="~~~~"):
+	 * Client 에서 b_username 이라는 변수에 값이 담겨서 
+	 * 전달되어 오면 username이라는 변수에 값을 저장하여
+	 * method내부로 전달하라
+	 * Client 와 Server 간에 변수 이름이 다를때 사용하는 선택사항
+	 */
+	
+	@RequestMapping(value = "/insert",method = RequestMethod.POST)
+	public String insert(
+			@RequestParam(value ="b_username") String username,
+			@RequestParam(value = "b_file") MultipartFile b_file,
+			Model model
+			) 
+	{
+		log.debug("사용자이름 : {} " , username);
+		log.debug(b_file.getOriginalFilename());
+		String fileName="";
+		
+		
+		try {
+			fileName= fileService.fileUp(b_file);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		model.addAttribute("FILE_NAME",fileName);
+		return "input";
+	}
+	
+	
+	@RequestMapping(value="/detail",method=RequestMethod.GET)
+	public String detail(String seq,Model model) {
+		
+		BBsDto bbsDto = bbsDao.findById(seq);
+		model.addAttribute("BBS",bbsDto);
+		return "detail";
+		
+	}
+	
+	
+	
+	
+}
