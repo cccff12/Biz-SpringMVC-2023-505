537ced6c5e26d64bad515ff94ce1ea84c0d55e9f
cccff12 <ksun0430@naver.com>
2023-07-31 10:37:13 +0900

    file

--- SpMVC_006_Files/src/main/java/com/callor/file/service/impl/BBsServiceImplV1.java
+++ SpMVC_006_Files/src/main/java/com/callor/file/service/impl/BBsServiceImplV1.java
@@ -1,5 +1,27 @@
 package com.callor.file.service.impl;
 
-public class BBsServiceImplV1 {
+import java.util.List;
 
+import com.callor.file.dao.BBsDao;
+import com.callor.file.model.BBsDto;
+import com.callor.file.service.BBsService;
+
+public class BBsServiceImplV1 implements BBsService {
+
+	protected final BBsDao bbsDao;
+
+	public BBsServiceImplV1(BBsDao bbsDao) {
+		super();
+		this.bbsDao = bbsDao;
+	}
+
+	@Override
+	public List<BBsDto> selecAll() {
+		
+		bbsDao.
+		
+		return null;
+	} 
+	
+	
 }
