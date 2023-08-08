6f7db057092b403214cdf4dfc42db638c45e311f
cccff12 <ksun0430@naver.com>
2023-08-08 16:52:02 +0900

    react랑연결

--- SpMVC_006_Files/document/20230731.sql
+++ SpMVC_006_Files/document/20230731.sql
@@ -2,6 +2,7 @@
 CREATE DATABASE imageDB;
 USE IMAGEDB;
 
+DROP table tbl_bbs;
 CREATE TABLE tbl_bbs(
 b_seq BIGINT PRIMARY key AUTO_INCREMENT,
 	b_title varchar(125),
@@ -14,3 +15,27 @@ b_seq BIGINT PRIMARY key AUTO_INCREMENT,
 	b_image VARCHAR(255),
 	b_origin_image VARCHAR(255)
 );
+
+
+show tables;
+
+DESC tbl_bbs;
+
+select * from tbl_bbs;
+
+
+TRUNCATE tbl_bbs;
+
+use imagedb;
+show tables;
+desc tbl_files;
+desc tbl_bbs;
+
+-- tbl_bbs.b_seq, tbl_files.f_bseq를 연결
+alter table tbl_files
+ADD CONSTRAINT F_BBS FOREIGN KEY (f_bseq)
+REFERENCES tbl_bbs(b_seq);
+
+
+
+
